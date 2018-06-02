b73f0fe718a1484e3743e84b891aa495fce7e128
Pierre Raoul <pierre@aktea.eu>
2018-06-02 08:17:13 +0200

    Ability to override with a local version

--- package.js
+++ package.js
@@ -1,5 +1,6 @@
 const path = Npm.require('path');
 const fs = Npm.require('fs');
+const runislandIonic4PackageName = 'runisland-ionic4';
 
 Package.describe({
   name: 'runisland:ionic4',
@@ -15,13 +16,53 @@ Package.onUse((api) => {
   api.addFiles('scriptIonic.html', 'client');
 
   // Within package.js we do not have access to `process` or `__dirname`, so we have to assume the path…
-  const files = getFilesRecursive('./dist');
+  const runislandIonic4PackagePath = isMeteorProject('.') ? getRunislandIonic4Package('.') : '.';
 
-  api.addAssets(files, 'client');
+  const distPath = runislandIonic4PackagePath ? path.join(runislandIonic4PackagePath, 'dist') : null;
+  const files = distPath ? getFilesRecursive(distPath) : null;
+  if (files) {
+    const fileRelPathes = files.map ((p) => {
+      return path.relative(runislandIonic4PackagePath, p);
+    })
+    api.addAssets(fileRelPathes, 'client');
+  } else {
+    console.log("Something went wrong: no 'dist' folder available.");
+  } 
+
 });
 
 // No test, only side effect of adding Ionic4 assets and loading ionic.js script.
 
+
+function isMeteorProject(source) {
+  return getChildren(source).indexOf('.meteor') > -1;
+}
+
+function getRunislandIonic4Package(source) {
+  const packagesPath = path.resolve(source, 'packages');
+  const packagePathes = getChildren(packagesPath);
+  return packagePathes.find((p) => {
+    if (!isDir(p)) {
+      return;
+    }
+
+    const packageJsonPath = path.resolve(p, 'package.json');
+    const result = isRunislandIonic4PackageJson(packageJsonPath);
+    return result;
+  })
+
+}
+
+function isRunislandIonic4PackageJson(source) {
+  if (! isFile(source)) {
+    return false;
+  }
+
+  const content = fs.readFileSync(source, 'utf8');
+  const jsonContent = JSON.parse(content);
+  const packageName = jsonContent.name;
+  return packageName === runislandIonic4PackageName;
+}
 
 function isDir(source) {
   return fs.lstatSync(source).isDirectory();
